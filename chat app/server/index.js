1c7a41056cdf9a92d119fe775f2ce4bef68327f5
harsh <harshtantia.20@gmail.com>
2024-02-23 16:22:27 +0530

    #

--- chat app/server/index.js
+++ chat app/server/index.js
@@ -1,2 +1,8 @@
 const http = require("http");
-const express = require("");
+const express = require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
