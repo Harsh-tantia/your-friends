18ee843ce9e9dd0e615cadda2ce34b77f3a5a080
Harsh Tantia <154208661+Harsh-tantia@users.noreply.github.com>
2024-02-26 14:03:06 +0530

    Update register.php

--- php redirection/register.php
+++ php redirection/register.php
@@ -246,27 +246,4 @@
 </script>
 
 </body>
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "<?php echo $apiKey; ?>",
-    authDomain: "<?php echo $authDomain; ?>",
-    projectId: "<?php echo $projectId; ?>",
-    storageBucket: "<?php echo $storageBucket; ?>",
-    messagingSenderId: "<?php echo $messagingSenderId; ?>",
-    appId: "<?php echo $appId; ?>",
-    measurementId: "<?php echo $measurementId; ?>"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
 </html>
