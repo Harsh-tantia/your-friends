60098563ff3ad20b7923f0a2c1e3ba77534a8a2d
harsh <harshtantia.20@gmail.com>
2024-03-04 14:04:58 +0530

    adding static admin page

--- chat app/server/index.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-const express = require("express");
-const cors = require("cors");
-const socketIO = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
